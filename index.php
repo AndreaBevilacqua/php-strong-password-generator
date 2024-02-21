33e932ba390b30dc280a2f6b622d87ae7ebb4995
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-21 19:10:29 +0100

    added index template

--- index.php
+++ index.php
@@ -7,9 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Password generator</title>
 </head>
-<body>
+<body class="container mt-5">
+    <h1 class="mb-4">Strong Password Generator</h1>
+    <form action="generate_password.php" method="get">
+        <div class="form-group">
+            <label for="passwordLength">Password Length:</label>
+            <input type="number" class="form-control" id="passwordLength" name="passwordLength" min="1" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Generate Password</button>
+</form>
     
 </body>
 </html>
