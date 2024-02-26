12d25f8a463be6a7ce05bde8189a5e1a72004206
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-26 18:41:42 +0100

    created success page

---
+++ success.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(isset($_SESSION['password'])){
+    $password = $_SESSION['password'];
+    session_destroy();
+} else {
+    header('Location: index.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="alert alert-info">
+            La password generata é : <strong><?= $password ?></strong>
+        </div>
+    
+</body>
+</html>
