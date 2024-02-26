d20d3e6918f6c058a99da287f3c3f5e396c3a5a4
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-26 18:25:21 +0100

    fixed function

--- index.php
+++ index.php
@@ -8,8 +8,17 @@ function generate_password($length){
     $symbols = '!@#$%^&*()_+-={}[]|:;,.?~';
     $characters = $letters . strtoupper($letters) . $numbers . $symbols;
 
+    $total_characters = mb_strlen($characters);
 
-    
+    while (mb_strlen($password) < $length ) {
+        $random_index = rand(0, $total_characters - 1);
+
+        $random_character = $characters[$random_index];
+
+        $password .= $random_character;
+    }
+
+    return $password;
 }
 ?>
 
